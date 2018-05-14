de81fa878ad8254819cb846003f663840560ce26
Hanumesha S <24047@ITCINFOTECH.COM>
2018-05-14 17:27:26 +0530

    First Time changes

---
+++ nike1/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from '../src/logo.svg';
+import '../src/App.css';
+import Home from './app/index';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to NikeReactJS Traning</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Home />
+      </div>
+    );
+  }
+}
+
+export default App;
